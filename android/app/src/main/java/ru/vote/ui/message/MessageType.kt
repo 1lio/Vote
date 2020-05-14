0a4b9ecc532eccf1bee26e1e5a173b1cde5bebdc
mandarin12325 <alioshin.evgenij@yandex.ru>
2020-05-14 17:55:17 +0300

    Добавлены константы типов сообщений

---
+++ android/app/src/main/java/ru/vote/ui/message/MessageType.kt
@@ -0,0 +1,11 @@
+package ru.vote.ui.message
+
+object MessageType {
+    const val CHECK_VOTE = 0
+    const val OK_VOTE = 1
+    const val TIME_VOTE = 2
+    const val NOT_VOTE = 3
+    const val DONE_VOTE = 4
+    const val PLANE_VOTE = 5
+
+}
