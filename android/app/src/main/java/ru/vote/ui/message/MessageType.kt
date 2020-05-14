929bba8b1a921724e1d3db616ee8e7e31350b85b
SukhovViacheslav <svo_efremov@mail.ru>
2020-05-14 21:16:08 +0300

    Todo: Добавить коментарии

--- android/app/src/main/java/ru/vote/ui/message/MessageType.kt
+++ android/app/src/main/java/ru/vote/ui/message/MessageType.kt
@@ -1,11 +1,12 @@
 package ru.vote.ui.message
 
 object MessageType {
+    // TODO: Добавит коментарии что означает конкретный тип
+
     const val CHECK_VOTE = 0
     const val OK_VOTE = 1
     const val TIME_VOTE = 2
     const val NOT_VOTE = 3
     const val DONE_VOTE = 4
     const val PLANE_VOTE = 5
-
 }
