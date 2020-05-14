df268fde6753c9dab29714a801af4dfdc3e3e42b
mandarin12325 <alioshin.evgenij@yandex.ru>
2020-05-14 17:57:52 +0300

    Добавлено фрагмент

--- android/app/src/main/java/ru/vote/MainActivity.kt
+++ android/app/src/main/java/ru/vote/MainActivity.kt
@@ -2,11 +2,13 @@ package ru.vote
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.vote.ui.message.MessageFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction().replace(R.id.host_activity, MessageFragment(4)).commit()
     }
 }
