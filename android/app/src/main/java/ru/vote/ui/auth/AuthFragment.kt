9ad43c6bce7c467a814529e6e96fe3233c83d251
ViacheslavSukhov <svo_efremov@mail.ru>
2020-05-15 12:48:14 +0300

    Обновлена Авторизация

--- android/app/src/main/java/ru/vote/ui/auth/AuthFragment.kt
+++ android/app/src/main/java/ru/vote/ui/auth/AuthFragment.kt
@@ -14,46 +14,62 @@ import kotlinx.android.synthetic.main.fr_auth.*
 import ru.vote.R
 import ru.vote.extensions.isValidLogin
 import ru.vote.extensions.isValidPass
+import ru.vote.repository.LocalRepository
 import ru.vote.ui.message.MessageFragment
 import ru.vote.viewmodel.AuthState
 import ru.vote.viewmodel.AuthViewModel
 
 class AuthFragment : Fragment() {
 
-    private lateinit var viewModel: AuthViewModel
+    private val localRepository = LocalRepository.getInstance()
     private var isValid = false
 
+    // Подготовительные фействия
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        retainInstance = true  // Сохраняем состояние фрагмента, живет до onDestroy
+
+    }
+
+    // Вызывается создание компонентов
     override fun onCreateView(inflater: LayoutInflater, group: ViewGroup?, state: Bundle?): View? {
         super.onCreateView(inflater, group, state)
         return inflater.inflate(R.layout.fr_auth, group, false)
     }
 
+    // Компаненты фрагмента созданы
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = ViewModelProvider(activity!!)[AuthViewModel::class.java]
+        loadLogin()
+        //   viewModel = ViewModelProvider(activity!!)[AuthViewModel::class.java]
         // Корректность данных
         isValid = (editLogin.isValidLogin() && editPassword.isValidPass())
-        viewModel.setValidForm(isValid)
+        // viewModel.setValidForm(isValid)
 
-        viewModel.observeForm(this, Observer {
-            viewModel.setEnableAction(isValid)
-        })
+        /*       viewModel.observeForm(this, Observer {
+                   viewModel.setEnableAction(isValid)
+               })*/
 
         // Чекаем поля при изменении
         listOf(editLogin, editPassword).forEach {
 
             it.addTextChangedListener(object : TextWatcher {
 
-                override fun afterTextChanged(s: Editable?) {}
-                override fun beforeTextChanged(s: CharSequence?, t: Int, c: Int, a: Int) {}
+                override fun afterTextChanged(s: Editable?) {
+                    editLogin.isValidLogin()
+                }
+
+                override fun beforeTextChanged(s: CharSequence?, t: Int, c: Int, a: Int) {
+                    editLogin.isValidLogin()
+                }
 
                 override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
 
-                    if (it == editPassword) editPassword.isValidLogin() else editPassword.isValidPass()
+                    //  if (it == editPassword) editPassword.isValidLogin() else editPassword.isValidPass()
 
                     isValid = (editPassword.isValidLogin() && editPassword.isValidPass())
-                    viewModel.setValidForm(isValid)
-                    viewModel.setEnableAction(isValid && (viewModel.getState() == AuthState.SIGN_IN))
+                    //     viewModel.setValidForm(isValid)
+                    //   viewModel.setEnableAction(isValid && (viewModel.getState() == AuthState.SIGN_IN))
 
                 }
             })
@@ -64,32 +80,55 @@ class AuthFragment : Fragment() {
             checkAuth(editLogin.text.toString(), editPassword.text.toString())
         }
 
+        checkboxLogin.setOnCheckedChangeListener { buttonView, isChecked ->
+            saveStateLogin(isChecked)
+            saveLogin(editLogin.text.toString())
+        }
     }
 
+    // Можем обращаться к активити
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+    // Фрагмент стал снова видимым
+    override fun onResume() {
+        super.onResume()
+    }
 
     private fun checkAuth(login: String, pass: String) {
 
-        activity!!.supportFragmentManager
+        val fm = activity!!.supportFragmentManager
             .beginTransaction()
             .addToBackStack("S")
-            .replace(R.id.host_activity, MessageFragment(0))
-            .commit()
 
+        val msgFR = MessageFragment(0)
+
+
+        fm.replace(R.id.host_activity, msgFR).commit()
 
         val handler = Handler()
         handler.postDelayed({
-            activity!!.supportFragmentManager
-                .beginTransaction()
-                .addToBackStack("S")
-                .replace(R.id.host_activity, AuthFragment())
-                .commit()
+            activity!!.onBackPressed()
         }, 3000)
         // Заглушка
         if (login == "ADMiN" && pass == "pass123") {
 
-
         }
     }
 
+    // Сохраненные данные
+    private fun saveStateLogin(state: Boolean) {
+        localRepository.isSavedLogin = state
+    }
 
+    private fun loadLogin() {
+        val isSaved =  localRepository.isSavedLogin
+        if (isSaved) editLogin.setText(localRepository.login)
+        checkboxLogin.isChecked = isSaved
+    }
+
+    private fun saveLogin(login: String) {
+        localRepository.login = login
+    }
 }
