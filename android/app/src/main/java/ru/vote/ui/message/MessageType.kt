421a4b6a0123331db6ede9517d94db25e017c68e
mandarin12325 <alioshin.evgenij@yandex.ru>
2020-05-17 12:37:21 +0300

    Добавлены коментарии

--- android/app/src/main/java/ru/vote/ui/message/MessageType.kt
+++ android/app/src/main/java/ru/vote/ui/message/MessageType.kt
@@ -1,12 +1,11 @@
 package ru.vote.ui.message
 
 object MessageType {
-    // TODO: Добавит коментарии что означает конкретный тип
 
-    const val CHECK_VOTE = 0
-    const val OK_VOTE = 1
-    const val TIME_VOTE = 2
-    const val NOT_VOTE = 3
-    const val DONE_VOTE = 4
-    const val PLANE_VOTE = 5
+    const val CHECK_VOTE = 0    // Проверка аутентификации
+    const val OK_VOTE = 1       // Проверка получение события
+    const val TIME_VOTE = 2     // Событие через определённое время
+    const val NOT_VOTE = 3      // Нет событий
+    const val DONE_VOTE = 4     // Пропущенно событие
+    const val PLANE_VOTE = 5    // Запланированно заседание
 }
