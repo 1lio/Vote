72edb8f2f97662ba70092321a405a1b6649c5b97
1lio <svoefremov@yandex.ru>
2020-11-03 00:40:21 +0300

    Add mainModule (Start Point)

---
+++ server/src/main/kotlin/ru/vote/Application.kt
@@ -0,0 +1,30 @@
+package ru.vote
+
+import io.ktor.application.Application
+import io.ktor.application.install
+import io.ktor.features.CallLogging
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.DefaultHeaders
+import io.ktor.gson.gson
+import io.ktor.server.netty.EngineMain
+import ru.vote.routes.addRouting
+
+import java.text.DateFormat
+
+fun main(args: Array<String>): Unit = EngineMain.main(args)
+
+fun Application.module() {
+
+    install(CallLogging)          // Включаем логирование
+    install(DefaultHeaders)       // Включаем заголовки (Date and Server)
+    install(ContentNegotiation) {
+
+        // Серилизация
+        gson {
+            setDateFormat(DateFormat.LONG)
+            setPrettyPrinting()
+        }
+    }
+
+    addRouting()            // Роутинг ("/", ets)
+}
